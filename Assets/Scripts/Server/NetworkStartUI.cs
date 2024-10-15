aa8b7cc76d34953f0fe5fe16a09de38252179d08
Omar Hernandez Salmeron <ohsalmeron@gmail.com>
2024-10-14 19:11:12 -0600

    Analytics

--- Assets/Scripts/Server/NetworkStartUI.cs
+++ Assets/Scripts/Server/NetworkStartUI.cs
@@ -117,7 +117,7 @@ public class NetworkStartUI : MonoBehaviour
             if (transport != null)
             {
                 // Set the client to connect to the server IP address
-                transport.SetConnectionData("74.208.246.177", 7777);
+                transport.SetConnectionData("127.0.0.1", 7777);
 
                 // Set client secure parameters for encrypted communication
                 transport.SetClientSecrets(SecureParameters.ServerCommonName, SecureParameters.MyGameClientCA);
